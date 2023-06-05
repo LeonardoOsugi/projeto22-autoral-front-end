8cc3af258002bf7ccc3ab7c146a2d7a3fbd4bf42
Leonardo Satoru Osugi <leoosugi96@gmail.com>
2023-06-04 22:44:59 -0300

    feat: initial settings

---
+++ src/pages/HomePage/HomePage.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export default function HomePage(){
+    return(
+        <>
+            <CaixaProdutos>
+                a
+                b
+            </CaixaProdutos>
+        </>
+    )
+};
+
+const CaixaProdutos = styled.div`
+        display: flex;
+`;
